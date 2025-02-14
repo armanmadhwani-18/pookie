446985d4667cedd8fa2ee07088e42175762e6a9d
armanmadhwani-18 <caffeinatededitors@gmail.com>
2025-02-14 08:16:38 +0000

    adf

--- valentine.js
+++ valentine.js
@@ -17,7 +17,7 @@ const heartImages = [
     const randomHeartImage = heartImages[Math.floor(Math.random() * heartImages.length)];
     heart.style.backgroundImage = `url(${randomHeartImage})`;
   
-    const size = Math.random() * 60 + 20; // Random size between 20px and 50px
+    const size = Math.random() * 100 + 30; // Random size between 20px and 50px
     heart.style.width = `${size}px`;
     heart.style.height = `${size}px`;
   
