0d66efa405b773c704c40c374084943967e7deb4
armanmadhwani-18 <caffeinatededitors@gmail.com>
2025-02-14 08:37:57 +0000

    dsa

--- script.js
+++ script.js
@@ -1,25 +1,37 @@
 // Function to generate random position for "No" button within the container
-function getRandomPosition(containerRect, buttonRect) {
-    const maxX = containerRect.width - buttonRect.width;
-    const maxY = containerRect.height - buttonRect.height;
+function getRandomPosition(containerRect, noButtonRect, yesButtonRect) {
+    const maxX = containerRect.width - noButtonRect.width;
+    const maxY = containerRect.height - noButtonRect.height;
   
-    const randomX = Math.random() * maxX;
-    const randomY = Math.random() * maxY;
+    let randomX, randomY;
+  
+    // Generate random positions until they don't overlap with the "Yes" button
+    do {
+      randomX = Math.random() * maxX;
+      randomY = Math.random() * maxY;
+    } while (
+      randomX < yesButtonRect.right &&
+      randomX + noButtonRect.width > yesButtonRect.left &&
+      randomY < yesButtonRect.bottom &&
+      randomY + noButtonRect.height > yesButtonRect.top
+    );
   
     return { x: randomX, y: randomY };
   }
   
   // Add event listener to "No" button
   const noButton = document.getElementById('noButton');
+  const yesButton = document.getElementById('yesButton');
   const container = document.querySelector('.container');
   
   noButton.addEventListener('mouseover', () => {
-    // Get the dimensions of the container and the button
+    // Get the dimensions of the container, "No" button, and "Yes" button
     const containerRect = container.getBoundingClientRect();
-    const buttonRect = noButton.getBoundingClientRect();
+    const noButtonRect = noButton.getBoundingClientRect();
+    const yesButtonRect = yesButton.getBoundingClientRect();
   
-    // Generate a random position within the container
-    const position = getRandomPosition(containerRect, buttonRect);
+    // Generate a random position within the container but avoid the "Yes" button
+    const position = getRandomPosition(containerRect, noButtonRect, yesButtonRect);
   
     // Update the button's position
     noButton.style.left = `${position.x}px`;
@@ -27,7 +39,6 @@ function getRandomPosition(containerRect, buttonRect) {
   });
   
   // Show custom loading GIF when "Yes" is clicked
-  const yesButton = document.getElementById('yesButton');
   const loadingContainer = document.getElementById('loading');
   
   yesButton.addEventListener('click', () => {
